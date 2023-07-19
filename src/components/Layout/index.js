96f2002d4e0780f6cc81da088aadc907f1d86fe8
Jacob Sniff <jacobsniff18@gmail.com>
2023-07-19 15:34:00 -0400

    Navbar completed

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,14 @@
-import './index.scss'
-import { Link } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/'
+import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import './index.scss';
 
-const Layout = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt="logo" />
-            <img src={LogoSubtitle} alt="sniff" />
-        </Link>
-    </div>
-)
+const Layout = () => {
+  return (
+    <>
+    <Sidebar />
+    </>
+  )
+}
 
-export default Layout;
+export default Layout
 
