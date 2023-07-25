7f0185c505c5ac5469dee3f949aaa63c7db98277
Jacob Sniff <jacobsniff18@gmail.com>
2023-07-25 15:29:12 -0400

    cleaned up code and zoomed out map by 2

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,14 +10,14 @@ const Layout = () => {
       <div className="page">
         {/* Using &lt; and &gt; is a way to display the angle brackets as regular text, without having them interpreted as HTML or JSX tags. 
       It's often used when you want to display code snippets or special characters on a webpage without them being executed as code.*/}
-        <span className="tags top-tags">&lt;body&gt;</span>
+        {/* <span className="tags top-tags">&lt;body&gt;</span> */}
 
         <Outlet />
 
         <span className="tags bottom-tags">
-          &lt;/body&gt; 
+          {/* &lt;/body&gt;  */}
         <br />
-        <span className='bottom-tag-html'>&lt;/html&gt;</span>
+        {/* <span className='bottom-tag-html'>&lt;/html&gt;</span> */}
         </span>
 
       </div>
