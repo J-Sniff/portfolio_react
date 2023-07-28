e7ee6c364009c81b59113e0a28455436e7ed8927
Jacob Sniff <jacobsniff18@gmail.com>
2023-07-27 21:30:00 -0400

    fixed routing issue for local. NEED to redeploy

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -9,18 +9,18 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => {
   return (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
+        <Link className='logo' to='/portfolio_react'>
             <img src={LogoS} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="sniff" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/" >
+            <NavLink exact="true" activeclassname="active" to="/portfolio_react" >
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about" >
+            <NavLink exact="true" activeclassname="active" className="about-link" to="about" >
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" >
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="contact" >
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
