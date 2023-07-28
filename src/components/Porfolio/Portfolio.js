4d7eeb18d265cf9d3ee03710df654b06a30bc5f5
Jacob Sniff <jacobsniff18@gmail.com>
2023-07-28 14:56:07 -0400

    added portfolio page to sidebar. Starting to create portfolio page

--- src/components/Porfolio/Portfolio.js
+++ src/components/Porfolio/Portfolio.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Portfolio.scss'
+// import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import { useState } from 'react'
 
 const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
   return (
-    <div>Portfolio</div>
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Portfolio'.split('')}
+            idx={15}
+          />
+        </h1>
+      </div>
+      {/* <Loader type="pacman" /> */}
+    </>
   )
 }
 
