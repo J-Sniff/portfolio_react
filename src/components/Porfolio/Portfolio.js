8883e57a607cad8b39522bccca03b06a6e649340
Jacob Sniff <jacobsniff18@gmail.com>
2023-07-29 11:54:23 -0400

    added more content to portfolio page. Creating data for projects

--- src/components/Porfolio/Portfolio.js
+++ src/components/Porfolio/Portfolio.js
@@ -17,6 +17,7 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
+        {/* <div>{renderPortfolio}</div> */}
       </div>
       {/* <Loader type="pacman" /> */}
     </>
