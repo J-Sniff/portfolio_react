a7563c1cef18250852a7617c73c03c5a8788902f
Jacob Sniff <jacobsniff18@gmail.com>
2023-07-30 16:37:40 -0400

    added data from two projects to json file and added styles to portfolio page. NEED to fix bug with images and discription.

--- src/components/Porfolio/Portfolio.js
+++ src/components/Porfolio/Portfolio.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import './Portfolio.scss'
-// import Loader from 'react-loaders'
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import portfolioData from '../../data/portfolio.json'
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  console.log(portfolioData)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  })
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.cover}
+                className="portfolio-image"
+                alt={port.title}
+              />
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 
   return (
     <>
@@ -17,9 +54,9 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
-        {/* <div>{renderPortfolio}</div> */}
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
-      {/* <Loader type="pacman" /> */}
+      <Loader type="pacman" />
     </>
   )
 }
